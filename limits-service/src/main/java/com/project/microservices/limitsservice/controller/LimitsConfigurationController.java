df4cbfa38629f2a315cc098c002355ebc99405f0
Jayash Kuchia <Jayash.Kuchia@PTPL.Local>
2020-09-06 19:33:45 +0530

    limit-services

---
+++ limits-service/src/main/java/com/project/microservices/limitsservice/controller/LimitsConfigurationController.java
@@ -0,0 +1,24 @@
+package com.project.microservices.limitsservice.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.project.microservices.limitsservice.bean.LimitConfiguration;
+import com.project.microservices.limitsservice.config.Configuration;
+
+import lombok.AllArgsConstructor;
+
+@RestController
+@AllArgsConstructor
+public class LimitsConfigurationController {
+
+	private Configuration configuration;
+
+	@GetMapping("/limits")
+	public LimitConfiguration retrieveLimitsFromConfigurations() {
+		LimitConfiguration limitConfiguration = new LimitConfiguration(configuration.getMaximum(), 
+				configuration.getMinimum());
+		return limitConfiguration;
+	}
+
+}
